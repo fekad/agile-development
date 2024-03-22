26b99792fba70679efe5a2b9c991dc570c1b04b9
Adam Fekete <fekad@users.noreply.github.com>
2024-03-22 11:34:30 +0100

    init

---
+++ src/myapp/__init__.py
@@ -0,0 +1,14 @@
+
+
+class MyApp():
+    def __init__(self) -> None:
+        print("Hello world!")
+        
+        
+    def feature(self):
+        print("New feature!")
+        
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.feature()
